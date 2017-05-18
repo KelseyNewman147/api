165adbd3a5d569c58a3fef92158a8e8f6d665dd2
Kelsey <kpnewman2013@gmail.com>
2017-05-18 11:11:28 -0400

    Add sort to cred transactions

--- app/controllers/cred_transactions_controller.rb
+++ app/controllers/cred_transactions_controller.rb
@@ -1,6 +1,6 @@
 class CredTransactionsController < ApplicationController
   def index
     user = User.find(params[:user_id])
-    render json: user.cred_transactions
+    render json: user.cred_transactions.order(created_at: :desc)
   end
 end
